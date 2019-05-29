e5289903e8a331bfa0599f65e9d1c5763bdf2a8c
tang <506180102@qq.com>
2019-05-29 12:35:27 +0800

    前台登录

--- src/main/java/com/one/clxj/controller/ReguserHandler.java
+++ src/main/java/com/one/clxj/controller/ReguserHandler.java
@@ -3,6 +3,7 @@ package com.one.clxj.controller;
 import com.one.clxj.pojo.Reguser;
 import com.one.clxj.pojo.ReguserExample;
 import com.one.clxj.service.ReguserSer;
+import com.one.clxj.util.miaodi.IndustrySMS;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class ReguserHandler {
 
     @Autowired
     private ReguserSer reguserSer;
+
 
     /**
      * 查看用户是否存在
@@ -67,4 +69,54 @@ public class ReguserHandler {
         }
         return  k;
     }
+    /**
+     * 发送验证码
+     */
+    @RequestMapping("/reguserYZM.do")
+    @ResponseBody
+    public boolean reguserYZM(String phone, String yzm){
+        try {
+            System.out.println("phone:"+phone+"  yzm="+yzm);
+            IndustrySMS.execute(phone,yzm);
+            return true;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * 用户短信
+     * @param reguser
+     * @return
+     */
+    @RequestMapping("/reguserLoginDX.do")
+    @ResponseBody
+    public int reguserLoginDX(Reguser reguser, HttpSession session,String yzm, String hqyzm){
+        System.out.println("reguser: "+reguser+" yzm="+yzm+" hqyzm="+hqyzm);
+        ReguserExample reguserExample = new ReguserExample();
+        ReguserExample.Criteria criteria = reguserExample.createCriteria();
+        criteria.andUsernameEqualTo(reguser.getUsername()); //加入手机号
+
+        int k=0; //存放登录状态  0 成功  1 验证码错误  2  账号冻结
+
+        if(hqyzm.equals(yzm)){ //判断验证码是否正确
+
+            List<Reguser> list = reguserSer.conditionFind(null,reguserExample);
+            System.out.println(list);
+
+            if (list.size()>0){  //判断用户是否存在
+                if (list.get(0).getEnableflag()==0){  //是否冻结
+                    k = 2;
+                }else{
+                    session.setAttribute("reguser",list.get(0));
+                }
+            }
+        }else {
+            k = 1;
+        }
+
+        return  k;
+    }
+
 }
